2fd0ab77dd74974f68eb0c3abc3eee5e872f3217
Siva nagu <t.sivanagu611@gmail.com>
2025-02-18 12:29:08 +0530

    Add the QR CODE Download Button

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-
-
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+      
        let imgBox = document.getElementById("imgBox");
        let qrImage = document.getElementById("qrImage");
        let qrText = document.getElementById("qrText");
-
+       let downloadBtn = document.getElementById("downloadBtn");
+       
        function generateQR(){
 
             if(qrText.value.length >0){
     
-                qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-    
+          // Fetching the API from goqr.me 
+                let qrCodeURL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
+
+                qrImage.src = qrCodeURL;
+
                 imgBox.classList.add("show-img");
+
+                downloadBtn.classList.add('download-active');
+          //Download the QR_IMG     
+                qrImage.onload = function () {
+
+                    fetch(qrCodeURL)
+                        .then(response => response.blob())
+                        .then(blob => {
+                            let qrBlobURL = URL.createObjectURL(blob);
+                            downloadBtn.href = qrBlobURL;
+                            downloadBtn.download = "QRCode.jpg";
+                           
+                        })
+                        .catch(error => console.error("Error downloading QR code:", error));
+                };
+
+
             }
            else{
             qrText.classList.add('error');
@@ -20,5 +41,6 @@
                 },1000);
     
            }
+
         
        }
